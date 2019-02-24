04e2a3afed64d552502285fb045a4f6398e9eafd
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2019-02-23 18:49:39 -0800

    1. Added runtime support for calculation of shifts inside domains

--- src/application/QRDecompositionApplication.cpp
+++ src/application/QRDecompositionApplication.cpp
@@ -6,14 +6,7 @@ using multifaq::params::NUM_OF_VARIABLES;
 std::string QRDecompApp::getCodeOfIncludes()
 {
     std::string includeStr = "";
-    includeStr += 
-           std::string("") 
-         + "#include \"../../LinearAlgebra/include/SVDecomp.h\"\n" 
-         + "#include <map>\n"
-         + "typedef std::map< std::tuple<unsigned int, long double, long double>, long double> DomainAggregate;\n"
-         + "typedef std::vector<DomainAggregate> VectorOffset;\n"
-         + "typedef std::map<unsigned int, VectorOffset> MapView;\n"
-         + "using namespace LMFAO::LinearAlgebra;\n\n";
+    includeStr += ""+ offset(0) + "#include \"../../LinearAlgebra/include/SVDecomp.h\"\n"+ offset(0) + "#include <map>\n"+ offset(0) + "#include <set>\n"+ offset(0) + "#include <unordered_map>\n"+ offset(0) + "\n"+ offset(0) + "typedef std::map< std::tuple<unsigned int, long double, long double>, long double> DomainAggregate;\n"+ offset(0) + "typedef std::vector<DomainAggregate> VectorOffset;\n"+ offset(0) + "typedef std::map<unsigned int, VectorOffset> MapView;\n"+ offset(0) + "\n"+ offset(0) + "typedef std::set<unsigned int> MapCategory;\n"+ offset(0) + "typedef std::vector<MapCategory> VectorDomain;\n"+ offset(0) + "\n"+ offset(0) + "typedef std::vector<std::unordered_map<unsigned int, unsigned int> > VectorShift;\n"+ offset(0) + "\n"+ offset(0) + "using namespace LMFAO::LinearAlgebra;\n";
      return includeStr;
 }
 
@@ -21,11 +14,10 @@ std::string QRDecompApp::getCodeOfFunDefinitions()
 {
     unsigned int numOfFeatures = _listOfFeatures.size() + 1;
     std::string numOfFeaturesStr = std::to_string(numOfFeatures);
-    std::string functionsString = ""+ offset(0) + "LMFAO::LinearAlgebra::MapMatrixAggregate mSigma;\n"+ offset(1) + "constexpr unsigned int LA_NUM_FEATURES = 33;\n"+ offset(0) + "\n"+ offset(1) + "unsigned int getTriangularIdx(unsigned int row, unsigned int col,\n"+ offset(11) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(11) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "unsigned int idxCur, idx, offset, offsetCat;\n"+ offset(2) + "if ((row == 0) && (col == 0))\n"+ offset(2) + "{\n"+ offset(3) + "return 0;\n"+ offset(2) + "}\n"+ offset(2) + "//offsetCat = vCatFeatCount[row + 1];\n"+ offset(2) + "if (row == 0)\n"+ offset(2) + "{\n"+ offset(3) + "row = col - 1;\n"+ offset(3) + "col = row;\n"+ offset(3) + "offsetCat = vCatFeatCount[row + 1] - vIsCatFeature[row + 1];\n"+ offset(2) + "}\n"+ offset(2) + "else\n"+ offset(2) + "{\n"+ offset(3) + "row = row - 1;\n"+ offset(3) + "offsetCat = vCatFeatCount[row + 1];\n"+ offset(2) + "}\n"+ offset(2) + "unsigned int n = LA_NUM_FEATURES;\n"+ offset(2) + "idxCur = n - row;\n"+ offset(2) + "idx = n * (n + 1) / 2 - idxCur * (idxCur + 1) / 2;\n"+ offset(2) + "//std :: cout << \"idx \" << idx << \" row \" << row << \" col \" << col << std::endl;\n"+ offset(2) + "offset = col - row - offsetCat;\n"+ offset(2) + "return idx + offset + 1;\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "const DomainAggregate &getCellValueUpper(unsigned int row, unsigned int col,\n"+ offset(14) + "MapView &mViews,\n"+ offset(14) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(14) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "unsigned int idx = getTriangularIdx(row, col, vIsCatFeature, vCatFeatCount);\n"+ offset(2) + "//std::cout << \"idx\" << idx << std::endl;\n"+ offset(2) + "const auto &viewOffset = vectorCofactorViews[idx];\n"+ offset(2) + "//std::cout << \"v \" << viewOffset.first << \" o \" << viewOffset.second << std::endl;\n"+ offset(2) + "return (mViews[viewOffset.first][viewOffset.second]);\n"+ offset(1) + "}\n"+ offset(1) + "const DomainAggregate &getCellValue(unsigned int row, unsigned int col,\n"+ offset(13) + "MapView &mViews,\n"+ offset(13) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(13) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "if (row > col)\n"+ offset(2) + "{\n"+ offset(3) + "std::swap(col, row);\n"+ offset(2) + "}\n"+ offset(2) + "return getCellValueUpper(row, col, mViews, vIsCatFeature, vCatFeatCount);\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(0) + "\n"+ offset(0) + "\n"+ offset(1) + "void printCell(unsigned int row, unsigned int col,\n"+ offset(6) + "const DomainAggregate& aggregate, char cellType)\n"+ offset(1) + "{\n"+ offset(2) + "switch (cellType)\n"+ offset(2) + "{\n"+ offset(3) + "case 'c':\n"+ offset(4) + "std::cout << row << \" \" <<  col << \" \" <<\n"+ offset(4) + "aggregate.begin()->second << std::endl;\n"+ offset(4) + "mSigma[std::make_pair(row, col)] =\n"+ offset(5) + "std::make_pair(aggregate.begin()->second, 0);\n"+ offset(5) + "break;\n"+ offset(2) + "}\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "void calculateCatFeatCount(const std::vector<bool> &vIsCatFeature,\n"+ offset(10) + "std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "vCatFeatCount.resize(vIsCatFeature.size());\n"+ offset(2) + "vCatFeatCount[0] = 0;\n"+ offset(2) + "for (unsigned int idx = 1; idx < vIsCatFeature.size(); idx ++)\n"+ offset(2) + "{\n"+ offset(4) + "vCatFeatCount[idx] = vCatFeatCount[idx - 1] + vIsCatFeature[idx];\n"+ offset(2) + "}\n"+ offset(1) + "}\n";
+    std::string functionsString = ""+ offset(1) + "LMFAO::LinearAlgebra::MapMatrixAggregate mSigma;\n"+ offset(1) + "constexpr unsigned int LA_NUM_FEATURES = 33;\n"+ offset(0) + "\n"+ offset(1) + "unsigned int getTriangularIdx(unsigned int row, unsigned int col,\n"+ offset(11) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(11) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "unsigned int idxCur, idx, offset, offsetCat;\n"+ offset(2) + "if ((row == 0) && (col == 0))\n"+ offset(2) + "{\n"+ offset(3) + "return 0;\n"+ offset(2) + "}\n"+ offset(2) + "//offsetCat = vCatFeatCount[row + 1];\n"+ offset(2) + "if (row == 0)\n"+ offset(2) + "{\n"+ offset(3) + "row = col - 1;\n"+ offset(3) + "col = row;\n"+ offset(3) + "offsetCat = vCatFeatCount[row + 1] - vIsCatFeature[row + 1];\n"+ offset(2) + "}\n"+ offset(2) + "else\n"+ offset(2) + "{\n"+ offset(3) + "row = row - 1;\n"+ offset(3) + "offsetCat = vCatFeatCount[row + 1];\n"+ offset(2) + "}\n"+ offset(2) + "unsigned int n = LA_NUM_FEATURES;\n"+ offset(2) + "idxCur = n - row;\n"+ offset(2) + "idx = n * (n + 1) / 2 - idxCur * (idxCur + 1) / 2;\n"+ offset(2) + "//std :: cout << \"idx \" << idx << \" row \" << row << \" col \" << col << std::endl;\n"+ offset(2) + "offset = col - row - offsetCat;\n"+ offset(2) + "return idx + offset + 1;\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "const DomainAggregate &getCellValueUpper(unsigned int row, unsigned int col,\n"+ offset(14) + "const MapView &mViews,\n"+ offset(14) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(14) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "unsigned int idx = getTriangularIdx(row, col, vIsCatFeature, vCatFeatCount);\n"+ offset(2) + "//std::cout << \"idx\" << idx << std::endl;\n"+ offset(2) + "const auto &viewOffset = vectorCofactorViews[idx];\n"+ offset(2) + "//std::cout << \"v \" << viewOffset.first << \" o \" << viewOffset.second << std::endl;\n"+ offset(2) + "return mViews.at(viewOffset.first)[viewOffset.second];\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "const DomainAggregate &getCellValue(unsigned int row, unsigned int col,\n"+ offset(13) + "const MapView &mViews,\n"+ offset(13) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(13) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "if (row > col)\n"+ offset(2) + "{\n"+ offset(3) + "std::swap(col, row);\n"+ offset(2) + "}\n"+ offset(2) + "return getCellValueUpper(row, col, mViews, vIsCatFeature, vCatFeatCount);\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "void printCell(unsigned int row, unsigned int col,\n"+ offset(6) + "const DomainAggregate& aggregate, char cellType)\n"+ offset(1) + "{\n"+ offset(2) + "switch (cellType)\n"+ offset(2) + "{\n"+ offset(3) + "case 'c':\n"+ offset(4) + "std::cout << row << \" \" <<  col << \" \" <<\n"+ offset(4) + "aggregate.begin()->second << std::endl;\n"+ offset(4) + "mSigma[std::make_pair(row, col)] =\n"+ offset(5) + "std::make_pair(aggregate.begin()->second, 0);\n"+ offset(5) + "break;\n"+ offset(2) + "}\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "void calculateCatFeatCount(const std::vector<bool> &vIsCatFeature,\n"+ offset(10) + "std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "vCatFeatCount.resize(vIsCatFeature.size());\n"+ offset(2) + "vCatFeatCount[0] = 0;\n"+ offset(2) + "for (unsigned int idx = 1; idx < vIsCatFeature.size(); idx ++)\n"+ offset(2) + "{\n"+ offset(4) + "vCatFeatCount[idx] = vCatFeatCount[idx - 1] + vIsCatFeature[idx];\n"+ offset(2) + "}\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "void calculateDomains(VectorDomain &vDomains,\n"+ offset(8) + "const MapView& mViews,\n"+ offset(8) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(8) + "const std::vector<unsigned int> &vCatFeatCount)\n"+ offset(1) + "{\n"+ offset(2) + "const unsigned int n = vIsCatFeature.size();\n"+ offset(2) + "for (unsigned int idx = 0; idx < n; idx ++)\n"+ offset(2) + "{\n"+ offset(4) + "vDomains.push_back(std::set<unsigned int>());\n"+ offset(2) + "}\n"+ offset(2) + "for (unsigned int row; row < n; row ++)\n"+ offset(2) + "{\n"+ offset(3) + "if (vIsCatFeature[row])\n"+ offset(3) + "{\n"+ offset(4) + "const DomainAggregate &m = getCellValue(row, row, mViews, vIsCatFeature, vCatFeatCount);\n"+ offset(4) + "for (const auto& it: m)\n"+ offset(4) + "{\n"+ offset(5) + "vDomains[row].insert(std::get<1>(it.first));\n"+ offset(4) + "}\n"+ offset(3) + "}\n"+ offset(2) + "}\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(0) + "\n"+ offset(1) + "void getDomainsSize(std::vector<unsigned int>& vDomainSize,\n"+ offset(8) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(8) + "const VectorDomain &vDomains)\n"+ offset(1) + "{\n"+ offset(2) + "const unsigned int n = vIsCatFeature.size();\n"+ offset(2) + "vDomainSize.resize(n);\n"+ offset(2) + "for (unsigned int idx = 0; idx < n; idx ++)\n"+ offset(2) + "{\n"+ offset(4) + "vDomainSize[idx] = 0;\n"+ offset(2) + "}\n"+ offset(2) + "for (unsigned int idx = 1; idx < n; idx ++)\n"+ offset(2) + "{\n"+ offset(4) + "vDomainSize[idx] = vDomainSize[idx - 1] + vDomains[idx].size()\n"+ offset(10) + "- 2 * vIsCatFeature[idx];\n"+ offset(2) + "}\n"+ offset(1) + "}\n"+ offset(0) + "\n"+ offset(1) + "void getCatShifts(VectorShift& vCatShifts,\n"+ offset(7) + "const std::vector<bool> &vIsCatFeature,\n"+ offset(7) + "const VectorDomain &vDomains)\n"+ offset(1) + "{\n"+ offset(2) + "const unsigned int n = vIsCatFeature.size();\n"+ offset(2) + "vCatShifts.resize(n);\n"+ offset(2) + "for (unsigned int idx = 1; idx < n; idx ++)\n"+ offset(2) + "{\n"+ offset(4) + "const MapCategory& mCategories = vDomains[idx];\n"+ offset(4) + "unsigned int shiftIdx = 0;\n"+ offset(4) + "for (const auto& it: mCategories)\n"+ offset(4) + "{\n"+ offset(5) + "vCatShifts[idx][it] = shiftIdx;\n"+ offset(5) + "shiftIdx++;\n"+ offset(4) + "}\n"+ offset(2) + "}\n"+ offset(1) + "}\n";
     return offset(1) + "std::vector< std::pair<unsigned int, unsigned int> > vectorCofactorViews;\n" 
             + functionsString;
 }
-
 
 inline std::string getViewName(size_t viewID)
 {
@@ -35,11 +27,10 @@ inline std::string getViewName(size_t viewID)
 std::string QRDecompApp::getVectorOfFeatures()
 {
     std::string returnString = "";
-    returnString += ""+ offset(2) + "std::vector<bool> vIsCatFeature;\n"
-                  + offset(2) + "std::vector<unsigned int> vCatFeatCount;\n";
+    returnString += ""+ offset(2) + "MapView mViews;\n"+ offset(2) + "VectorDomain vDomains;\n"+ offset(2) + "std::vector<unsigned int> vDomainSize;\n"+ offset(2) + "VectorShift vCatShifts;\n"+ offset(2) + "std::vector<bool> vIsCatFeature;\n"+ offset(2) + "std::vector<unsigned int> vCatFeatCount;\n\n";
+    
     // Intercept feature. 
-    returnString +=  
-                offset(2) + "vIsCatFeature.push_back(false);\n";
+    returnString +=  offset(2) + "vIsCatFeature.push_back(false);\n";
     for (size_t var = 0; var < NUM_OF_VARIABLES; var ++)
     {
         if (_features[var])
@@ -52,29 +43,23 @@ std::string QRDecompApp::getVectorOfFeatures()
 
         }
     }
-    returnString += offset(2) + "calculateCatFeatCount(vIsCatFeature, vCatFeatCount);\n";
+    returnString += offset(2) + "calculateCatFeatCount(vIsCatFeature, vCatFeatCount);\n\n";
     return returnString;
 }
 
 std::string QRDecompApp::getCodeOfRunAppFun()
 {
-    //std::string dumpListOfQueries = "";
     std::string vectorCofactorViews = "";
 
     for (Query* query : listOfQueries)
     {
         std::pair<size_t,size_t>& viewAggPair = query->_aggregates[0]->_incoming[0];
-        /*       
-        dumpListOfQueries += std::to_string(viewAggPair.first)+","+
-            std::to_string(viewAggPair.second)+"\\n";
-        */
         vectorCofactorViews += 
         offset(2) + "vectorCofactorViews.push_back(std::make_pair(" 
         + std::to_string(viewAggPair.first) + "," + 
         std::to_string(viewAggPair.second) + "));\n";
     }
     
-
     std::string returnString = "";
 
     for (size_t viewID = 0; viewID < _compiler->numberOfViews(); ++viewID)
@@ -122,18 +107,15 @@ std::string QRDecompApp::getCodeOfRunAppFun()
         returnString += offset(2) + "{\n" + calcViews + 
                         offset(2) + "}\n";
     }
-
-    std::string dumpCovarianceString = ""+ offset(2) + "for (unsigned int row = 0; row < LA_NUM_FEATURES; row++)\n"+ offset(2) + "{\n"+ offset(3) + "for (unsigned int col = 0; col < LA_NUM_FEATURES; col++)\n"+ offset(3) + "{\n"+ offset(4) + "const auto &m = getCellValue(row, col, mViews, vIsCatFeature, vCatFeatCount);\n"+ offset(4) + "char cellType;\n"+ offset(4) + "if (!vIsCatFeature[row] && !vIsCatFeature[col])\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'c';\n"+ offset(4) + "}\n"+ offset(4) + "else if (vIsCatFeature[row] && !vIsCatFeature[col])\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'k';\n"+ offset(4) + "}\n"+ offset(4) + "else if (!vIsCatFeature[row] && vIsCatFeature[col])\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'r';\n"+ offset(4) + "}\n"+ offset(4) + "else if (vIsCatFeature[row] &&  vIsCatFeature[col]\n"+ offset(6) + "&& (row == col))\n"+ offset(4) + "{\n"+ offset(6) + "cellType == 'd';\n"+ offset(4) + "}\n"+ offset(4) + "else\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'm';\n"+ offset(4) + "}\n"+ offset(4) + "printCell(row, col, m, cellType);\n"+ offset(3) + "}\n"+ offset(2) + "}\n"+ offset(2) + "//SVDecomp svdDecomp(SVDecomp::DecompType::NAIVE, mSigma, LA_NUM_FEATURES);\n"+ offset(2) + "//svdDecomp.decompose()\n";
-    returnString += dumpCovarianceString;
-    std::string runFunction = offset(1)+"void runApplication()\n"+offset(1)+"{\n"
-        //offset(2)+"std::ofstream ofs(\"output/covarianceMatrix.out\");\n"+
+    std::string calculateDomains = ""+ offset(2) + "calculateDomains(vDomains, mViews, vIsCatFeature, vCatFeatCount);\n"+ offset(2) + "getDomainsSize(vDomainSize, vIsCatFeature, vDomains);\n"+ offset(2) + "getCatShifts(vCatShifts, vIsCatFeature, vDomains);\n\n";
+    std::string generateQR = ""+ offset(2) + "for (unsigned int row = 0; row < LA_NUM_FEATURES; row++)\n"+ offset(2) + "{\n"+ offset(3) + "for (unsigned int col = 0; col < LA_NUM_FEATURES; col++)\n"+ offset(3) + "{\n"+ offset(4) + "const auto &m = getCellValue(row, col, mViews, vIsCatFeature, vCatFeatCount);\n"+ offset(4) + "char cellType;\n"+ offset(4) + "if (!vIsCatFeature[row] && !vIsCatFeature[col])\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'c';\n"+ offset(4) + "}\n"+ offset(4) + "else if (vIsCatFeature[row] && !vIsCatFeature[col])\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'k';\n"+ offset(4) + "}\n"+ offset(4) + "else if (!vIsCatFeature[row] && vIsCatFeature[col])\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'r';\n"+ offset(4) + "}\n"+ offset(4) + "else if (vIsCatFeature[row] &&  vIsCatFeature[col]\n"+ offset(6) + "&& (row == col))\n"+ offset(4) + "{\n"+ offset(6) + "cellType == 'd';\n"+ offset(4) + "}\n"+ offset(4) + "else\n"+ offset(4) + "{\n"+ offset(6) + "cellType = 'm';\n"+ offset(4) + "}\n"+ offset(4) + "printCell(row, col, m, cellType);\n"+ offset(3) + "}\n"+ offset(2) + "}\n"+ offset(2) + "//SVDecomp svdDecomp(SVDecomp::DecompType::NAIVE, mSigma, LA_NUM_FEATURES);\n"+ offset(2) + "//svdDecomp.decompose()\n";
+    returnString += calculateDomains + generateQR;
+    std::string runFunction = 
+         offset(1) + "void runApplication()\n" 
+         + offset(1) + "{\n"
          + getVectorOfFeatures() 
-         + vectorCofactorViews + "\n"+
-         offset(2) + "MapView mViews;\n" + 
-        //offset(2)+"ofs << \""+dumpListOfQueries+"\";\n"+
-        //offset(2)+"ofs.close();\n\n"+
-         returnString + 
-
-        offset(1)+"}\n";
+         + vectorCofactorViews + "\n"
+         + returnString 
+         + offset(1) + "}\n";
     return runFunction;
 }
