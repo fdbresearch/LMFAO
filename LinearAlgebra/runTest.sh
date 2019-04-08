3f77aeea644a8942ab5b42cf789f2272f83839cd
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2019-04-09 00:37:08 +0100

    1. Added a script for measuring performance on different datasets

---
+++ LinearAlgebra/runTest.sh
@@ -0,0 +1,30 @@
+#DFDB_SH_DATA="/media/popina/test/dfdb/benchmarking/datasets"
+DFDB_SH_ROOT="/media/popina/test/dfdb/LMFAO"
+#DFDB_SH_ROOT="/home/popina/Documents/FDB/LMFAO"
+DFDB_SH_LA="$DFDB_SH_ROOT/LinearAlgebra"
+DFDB_SH_DATA="$DFDB_SH_ROOT/data"
+DFDB_SH_RUNTIME_CPP="$DFDB_SH_ROOT/runtime/cpp"
+DFDB_SH_RUNTIME_OUTPUT="$DFDB_SH_RUNTIME_CPP/output"
+DFDB_SH_DATA_SET=$1
+#"usretailer_1"
+cd $DFDB_SH_LA
+: '
+cmake .
+make -j8
+'
+cd $DFDB_SH_ROOT
+cmake .
+make -j8
+./multifaq --path "data/${DFDB_SH_DATA_SET}/" --model covar --parallel both
+./multifaq --path "data/${DFDB_SH_DATA_SET}/" --model covar --codegen sql
+: '
+cd $DFDB_SH_RUNTIME_OUTPUT
+rm *
+cd $DFDB_SH_RUNTIME_CPP
+make dump -j8
+./lmfao
+cd $DFDB_SH_LA
+python3 ./scripts/transform.py -o $DFDB_SH_RUNTIME_OUTPUT -d "$DFDB_SH_DATA/$DFDB_SH_DATA_SET/" >test.in
+./lmfaola
+'
+
--- LinearAlgebra/runTests.sh
+++ LinearAlgebra/runTests.sh
@@ -1,28 +1,8 @@
-#DFDB_SH_DATA="/media/popina/test/dfdb/benchmarking/datasets"
-#DFDB_SH_ROOT="/media/popina/test/dfdb/LMFAO"
-DFDB_SH_ROOT="/home/popina/Documents/FDB/LMFAO"
-DFDB_SH_LA="$DFDB_SH_ROOT/LinearAlgebra"
-DFDB_SH_DATA="$DFDB_SH_ROOT/data"
-DFDB_SH_RUNTIME_CPP="$DFDB_SH_ROOT/runtime/cpp"
-DFDB_SH_RUNTIME_OUTPUT="$DFDB_SH_RUNTIME_CPP/output"
-cd $DFDB_SH_LA
-: '
-cmake .
-make -j8
-'
-cd $DFDB_SH_ROOT
-cmake .
-make -j8
-./multifaq --path "data/usretailer/" --model qrdecomp --parallel both
-cd $DFDB_SH_RUNTIME_OUTPUT
-rm *
-cd $DFDB_SH_RUNTIME_CPP
-make dump -j8
-./lmfao
-: '
-cd $DFDB_SH_LA
-DFDB_SH_DATA_SET="usretailer"
-python3 ./scripts/transform.py -o $DFDB_SH_RUNTIME_OUTPUT -d "$DFDB_SH_DATA/$DFDB_SH_DATA_SET/" >test.in
-./lmfaola
-'
+#!/bin/bash
 
+data_sets=(usretailer_1 usretailer_10 usretailer_100 usretailer_1000 usretailer)
+for data_set in ${data_sets[@]}; do
+    echo data_set_name: "$data_set"; 
+    log_name=log"${data_set}".txt
+    sh runTest.sh ${data_set} > ${log_name}
+done
