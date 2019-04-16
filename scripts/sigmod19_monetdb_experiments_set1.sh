0e890aca582fa3d4170895ea65bc36b9e6b749d6
Ubuntu <ubuntu@ip-172-31-16-88.eu-west-2.compute.internal>
2019-04-16 09:21:34 +0000

    updated monetdb script

--- scripts/sigmod19_monetdb_experiments_set1.sh
+++ scripts/sigmod19_monetdb_experiments_set1.sh
@@ -7,17 +7,17 @@ CLEANUP_SCRIPT=aggregate_cleanup.sql
 
 echo "$SQL_SCRIPT"
 
-for dataset in retailer favorita yelp; 
+for dataset in retailer favorita tpc-ds yelp; 
 do 
 	mclient -d sigmod19 < ~/benchmarking/datasets/"$dataset"/monetdb/load_data.sql 
 
 	for model in covar count rtree mi cube;
 	do 
-		LOG_FILE=sigmod19_experiments_set1_$dataset_$model_monetdb.log
+		LOG_FILE=sigmod19_experiments_set1_"$dataset"_"$model"_monetdb.log
 
 		echo " MonetDB -- sql -- $dataset -- $model" >> runtime/"$LOG_FILE"
 
-		./multifaq --path ../benchmarking/datasets/"$dataset" --model "$model" --codegen sql --feat config_sigmod19/features_"$model".conf >> log_multifaq_sigmod19_experiments_set1_$dataset_$model_monetdb.log
+		./multifaq --path ../benchmarking/datasets/"$dataset" --model "$model" --codegen sql --feat config_sigmod19/features_"$model".conf >> log_multifaq_sigmod19_experiments_set1_"$dataset"_"$model"_monetdb.log
 
 		cat compiler-data.out >> runtime/"$LOG_FILE"
 
@@ -28,9 +28,9 @@ do
 
 		echo "Run $SQL_SCRIPT - MonetDB - $dataset - $mdel " >> ../"$LOG_FILE"
 
-		for i in {1..5}
+		for i in {1..3}
 		do
-		      echo "Run $i $SQL_SCRIPT  - MonetDB - $dataset - $model" >> ../../log_multifaq_sigmod19_experiments_set1_$dataset_$model_monetdb.log
+		      echo "Run $i $SQL_SCRIPT  - MonetDB - $dataset - $model" >> ../../log_multifaq_sigmod19_experiments_set1_"$dataset"_"$model"_monetdb.log
 		      /usr/bin/time -f "%e %P %I %O" -o "times.txt" -a mclient -d sigmod19 < $SQL_SCRIPT > /dev/null
 		      mclient -d sigmod19 < $CLEANUP_SCRIPT > /dev/null
 		done
