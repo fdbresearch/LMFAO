cb0565d0f08f8cbaed06b6320e3a1e01fc9444fb
popina1994 <djordjezivanovicva@gmail.com>
2019-06-06 16:35:25 +0100

    1. Added support for verbose linkage information of eigen. 2. Added new favorita test. 3. Outputing count of table + database size in files.

--- LA-F/scripts/generate_join.sh
+++ LA-F/scripts/generate_join.sh
@@ -5,6 +5,7 @@ cd $DFDB_SH_LMFAO_P
 ./multifaq --path "${DFDB_SH_DATA}/${DFDB_SH_DATA_SET}/" --model covar --codegen sql
 
 cd ${DFDB_SH_RUNTIME_SQL}
+
 eval_time "Loading data" psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -f load_data.sql
 
 function clean_up_impl() {
@@ -12,8 +13,15 @@ function clean_up_impl() {
 }
 
 function run_test_impl() {
+    psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -c    \
+           "SELECT pg_size_pretty(pg_database_size('${DFDB_SH_DB}')) AS size_in_mb"
+    psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -c    \
+                   "SELECT pg_database_size('${DFDB_SH_DB}') / 1024 / 1024 || 'MB' AS size_in_mb;"
     eval_time "Joining data" psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -f join_features.sql
     eval_time "Exporting data" psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -f export.sql
+    psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -c    \
+                               "SELECT pg_database_size('${DFDB_SH_DB}') / 1024 / 1024 || 'MB' AS size_in_mb; "  
+    psql -U $DFDB_SH_USERNAME -p $DFDB_SH_PORT -d ${DFDB_SH_DB} -f  count_tuples.sql
 }
 
 
--- LA-F/scripts/la_eigen.sh
+++ LA-F/scripts/la_eigen.sh
@@ -12,7 +12,7 @@ echo "Dump" $DFDB_SH_DUMP
 g++ -std=c++14 -O3 -pthread -mtune=native -ftree-vectorize \
 -fopenmp  -DEIGEN_USE_BLAS -DEIGEN_USE_LAPACKE             \
 lmfao_la_eigen.cpp -lboost_program_options                 \
--lopenblas -llapacke  -o lmfao_la_eigen
+-lopenblas -llapacke  -Wl,--verbose -o lmfao_la_eigen
 eval ${DFDB_TIME} ./lmfao_la_eigen -p $DFDB_SH_JOIN_RES_PATH -o "$data_operation"        \
 -f "${features}" -c "${features_cat}" --dump "${DFDB_SH_DUMP}" --dump_path "$dump_eigen" \
 --num_it "${DFDB_SH_NUM_REP}"
